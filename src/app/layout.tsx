18596e5e65d17d4a788bff34c1a103e10e987d61
David Inoa <davidinoa@gmail.com>
2023-12-14 22:43:37 -0500

    feat: temporarily remove navbar; minor seo improvements

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,10 +8,10 @@ import '@/styles/tailwind.css'
 export const metadata: Metadata = {
   title: {
     template: '%s - David Inoa',
-    default: 'David Inoa - Software designer, founder, and amateur astronaut',
+    default: 'David Inoa - Web Engineer',
   },
   description:
-    'I’m David, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms.',
+    'Portfolio of David Inoa, a web engineer based in the United States.',
   alternates: {
     types: {
       'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -437,10 +437,10 @@ export function Header() {
                   </AvatarContainer>
                 )}
               </div>
-              <div className="flex flex-1 justify-end md:justify-center">
+              {/* <div className="flex flex-1 justify-end md:justify-center">
                 <MobileNavigation className="pointer-events-auto md:hidden" />
                 <DesktopNavigation className="pointer-events-auto hidden md:block" />
-              </div>
+              </div> */}
               <div className="flex justify-end md:flex-1">
                 <div className="pointer-events-auto">
                   <ThemeToggle />
