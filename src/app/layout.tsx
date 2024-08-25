8a64b0518db5157f92f9ef2532af2bed4c941ecf
David Inoa <davidinoa@gmail.com>
2024-08-25 19:45:35 -0400

    feat: configure PostHog for analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Providers } from '@/app/providers'
 import { Layout } from '@/components/Layout'
 
 import '@/styles/tailwind.css'
+import { PHProvider } from './analytics/provider'
 
 export const metadata: Metadata = {
   title: {
@@ -25,17 +26,19 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className="h-full antialiased" suppressHydrationWarning>
-      <body
-        className="flex h-full bg-zinc-50 dark:bg-black"
-        suppressHydrationWarning
-      >
-        <Providers>
-          <div className="flex w-full">
-            <Layout>{children}</Layout>
-          </div>
-        </Providers>
-      </body>
-    </html>
+    <PHProvider>
+      <html lang="en" className="h-full antialiased" suppressHydrationWarning>
+        <body
+          className="flex h-full bg-zinc-50 dark:bg-black"
+          suppressHydrationWarning
+        >
+          <Providers>
+            <div className="flex w-full">
+              <Layout>{children}</Layout>
+            </div>
+          </Providers>
+        </body>
+      </html>
+    </PHProvider>
   )
 }
